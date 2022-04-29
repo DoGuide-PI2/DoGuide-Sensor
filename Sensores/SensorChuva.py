1bf792d46fc7d68a7c74cf01f8ba6f05c28e39b9
Adriana <99109985+adrianaguiar@users.noreply.github.com>
2022-04-29 17:15:49 -0300

    Update SensorChuva.py

--- Sensores/SensorChuva.py
+++ Sensores/SensorChuva.py
@@ -5,10 +5,11 @@ d = Pin(32, Pin.IN) #Pino 32 como entrada
 
 def loopreading():
     while True:
-        if d.value() > 0:
+        if d.value() > 0: #A detecção de chuva funcionará quando for detectado nível baixo
             x = "No Rain"
         else:
             x = "Rain"
+            return 'm'   #A string de retorno para o caso da identificação de chuva. Se não for detectado, retornará ao loop de leitura.
     sleep(1)
      
 loopreading()
